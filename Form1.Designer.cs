2be6088743e54737dca2993db6c36a907b524bf5
felixmaker <1162850692@qq.com>
2023-05-09 10:21:50 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace SetExplorerOpenLocation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择「目录」作为任务栏文件资源管理器打开目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SetOpenFolder);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(27, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 75);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "运行时确保任务栏已经固定「文件资源管理器」，否则可能出现奇怪的错误。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 219);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "设置任务栏";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SetExplorerOpenLocation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static IWshRuntimeLibrary.IWshShortcut CreateShortcut(string pathLink, string pathTarget)
+        {
+            var shell = new IWshRuntimeLibrary.WshShell();
+
+            if (!pathLink.EndsWith(".lnk"))
+            {
+                throw new NotSupportedException("快捷方式只支持以「.lnk」作为结尾");
+            }
+
+            var shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(pathLink);
+            shortcut.TargetPath = pathTarget;
+            return shortcut;
+        }
+
+        public static void CreateExplorerLink(string pathTarget)
+        {
+            // %USERPROFILE%\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar
+
+            var applicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var pathLink = System.IO.Path.Combine(applicationData, "Microsoft\\Internet Explorer\\Quick Launch\\User Pinned\\TaskBar\\File Explorer.lnk");
+
+            if (!System.IO.File.Exists(pathLink))
+            {
+                throw new System.IO.FileNotFoundException("请先确保任务栏已固定「文件资源管理器」再进行操作！");
+            }
+
+            var shortcut = CreateShortcut(pathLink, pathTarget);
+            shortcut.Save();
+        }
+
+        private string SelectFolder()
+        {
+            folderBrowserDialog1.SelectedPath = "";
+            folderBrowserDialog1.ShowDialog();
+
+            if (folderBrowserDialog1.SelectedPath == "")
+            {
+                throw new System.IO.DirectoryNotFoundException("请选择「目录」");
+            }
+
+            return folderBrowserDialog1.SelectedPath;
+        }
+
+        private void SetOpenFolder(object sender, EventArgs e)
+        {
+            try
+            {
+                var target = SelectFolder();
+                CreateExplorerLink(target);
+
+                MessageBox.Show($"设置成功，现在的打开位置为：\n「{target}」", "设置成功");
+            }
+            catch (System.IO.DirectoryNotFoundException error)
+            {
+                MessageBox.Show(error.Message, "目录不存在");
+            }
+            catch (System.IO.FileNotFoundException error)
+            {
+                MessageBox.Show(error.Message, "文件不存在");
+            }
+            catch (NotSupportedException error)
+            {
+                MessageBox.Show(error.Message, "失败");
+            }
+        }
+    }
+}
